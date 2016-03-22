85d0050cde5499c07c531cc5ed6f3d5ccb8e6657
AnousoneO <anousone.o@outlook.fr>
2016-03-22 10:00:15 +0100

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -1,20 +1,35 @@
 <?php
-    $login=$_POST["login"];
-    $email=$_POST["email"];
-    //retourne un nombre hexadecimal de 32 caractères pour éviter d'enregistrer les mdp des utilisateurs
-    $mdpConnex=md5($_POST["mdp"]); //pour la page connexion
-    $mdpInscri=md5($_POST["mdp"]); //pour la page déconnexion
-    //$bdd = new PDO('mysql:host=shakinboiz3.mysql.db;dbname=shakinboiz3;charset=utf8', 'shakinboiz3', '3Bananes');
-    $user="shakinboiz3";
-    $passwd="3Bananes";
-    $host="shakinboiz3.mysql.db";
-    $bdd="shakinboiz3";
-    $connexion=mysqli_connect($host, $user, $passwd) or die ("connexion refusee");
-    mysqli_select_db($connexion, $bdd);
+    // on se connecte à MySQL 
+    $db = mysql_connect('shakinboiz3.mysql.db', 'shakinboiz3', '3Bananes'); 
+    mysql_select_db('shakinboiz3',$db); 
 
-    $insert="INSERT INTO utilisateur VALUES ('', '$login', '$mdpInscri', '', '', '$email', '')";
-    mysqli_query($connexion, $insert) or die ("Connexion nulle");
-    
-    header('location:../filactu');
+    if(isset($_POST) && !empty($_POST['login']) && !empty($_POST['password']) && !empty($_POST['email'])) {
+        extract($_POST);
+        $password = sha1($_POST['password']);
+      
+        // on recupére le password de la table qui correspond au login du visiteur
+        $sql = "select id_util, mail_util from utilisateur where login_util='".$login."'";
+        $req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+
+        $data = mysql_fetch_assoc($req);
+
+        if($data['id_util'] > 0) {
+          echo '<div class="alert alert-dismissable alert-danger">
+        <button type="button" class="close" data-dismiss="alert">x</button>
+        <strong>Oh Non !</strong> Le login existe déjà. Merci de recommencer !
+      </div>';
+      var_dump($data);
+        }
+        
+        else {
+          mysql_query("INSERT INTO utilisateur VALUES('','$login','$password', '', '', '$email', '')");
+          session_start();
+          $_SESSION['login'] = $login;
+          header('location:../filactu');
+        }    
+      }
+    else {
+      $champs = '<p><b>(Remplissez tous les champs pour vous connectez !)</b></p>';
+    }
 
 ?>
