6fcbc16ddbd85e37312d38040e611285b1f5767a
AnousoneO <anousone.o@outlook.fr>
2016-03-25 15:47:04 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -4,29 +4,34 @@
     mysql_select_db('shakinboiz3',$db); 
 
     if(isset($_POST) && !empty($_POST['login']) && !empty($_POST['password'])) {
-        extract($_POST);
-        $password = sha1($_POST['password']);
+    extract($_POST);
+    $password = sha1($_POST['password']);
       
-        // on recupère le password de la table qui correspond au login du visiteur
-        $sql = "select mdp_util from utilisateur where login_util='".$login."'";
-        $req = mysql_query($sql) or die ('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+      // on recupére le password de la table qui correspond au login du visiteur
+      $sql = "select mdp_util from utilisateur where login_util='".$login."'";
+      $req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
 
-        $data = mysql_fetch_assoc($req);
+      $data = mysql_fetch_assoc($req);
 
-        if($data['mdp_util'] != $password) {
-          echo '<div class="alert alert-dismissable alert-danger">
-        <button type="button" class="close" data-dismiss="alert">x</button>
-        <strong>Oh Non !</strong> Mauvais login / password. Merci de recommencer !
-      </div>';
-        }
-        
-        else {
-          session_start();
-          $_SESSION['login'] = $login;
-
-          header('location:../filactu');
-        }    
+      if($data['mdp_util'] != $password) {
+        echo '<div class="alert alert-dismissable alert-danger">
+      <button type="button" class="close" data-dismiss="alert">x</button>
+      <strong>Oh Non !</strong> Mauvais login / password. Merci de recommencer !
+    </div>';
       }
+      
+      else {
+        session_start();
+        $_SESSION['login'] = $login;
+        
+        echo '<div class="alert alert-dismissable alert-success">
+      <button type="button" class="close" data-dismiss="alert">×</button>
+      <strong>Yes !</strong> Vous etes bien logué, Redirection dans 5 secondes ! <meta http-equiv="refresh" content="5; URL=../filactu">
+    </div>';
+        // ici vous pouvez afficher un lien pour renvoyer
+        // vers la page d'accueil de votre espace membres 
+      }    
+    }
     else {
       $champs = '<p><b>(Remplissez tous les champs pour vous connectez !)</b></p>';
     }
